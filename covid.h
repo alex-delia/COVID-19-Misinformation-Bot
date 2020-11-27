78a86d895365d10f00126c2831d0e9aefd5240aa
alex-delia <adelia3@uwo.ca>
2020-11-26 20:43:49 -0500

    Restructured covid.cpp to be more efficient and add Canada daily deaths/recoveries

--- covid.h
+++ covid.h
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "json.hpp"
+
 using namespace std;
 
 class covid {
     public:
         static size_t write_to_string(void *ptr, size_t size, size_t count, void *stream);
         static string getWorldData();
+        static nlohmann::json getCanadaData();
         static string getCanadaTests();
+        static string getCanadaDeaths();
+        static string getCanadaRecoveries();
+
 };
